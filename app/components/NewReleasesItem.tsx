f4bc0c8640462c1e072df891e735d45f641c5d78
Luis Mas Serral <luismasserral@Luiss-MacBook-Pro.local>
2023-05-01 18:45:01 +0200

    Added navigation on new releases home widget

--- app/components/NewReleasesItem.tsx
+++ app/components/NewReleasesItem.tsx
@@ -1,5 +1,11 @@
+import {useRecordItemInteractions} from '@crossingminds/beam-react'
 import type {ProductVariant} from '@shopify/hydrogen/storefront-api-types'
 import type {FunctionComponent} from 'react'
+import {useMemo} from 'react'
+
+import {BEAM_REACT_OPTIONS} from '~/beam/config'
+import {sessionId} from '~/utils/sessionId.client'
+import {SHOPIFY_ENTITY_TYPES, getIdFromShopifyEntityId} from '~/utils/shopify'
 
 import {
   newReleasesItemStyle,
@@ -13,16 +19,37 @@ interface NewReleasesItemProps {
 export const NewReleasesItem: FunctionComponent<NewReleasesItemProps> = ({
   productVariant
 }) => {
+  const {recordItemClickInteraction} = useRecordItemInteractions({
+    ...BEAM_REACT_OPTIONS,
+    sessionId
+  })
+
+  const productVariantId = useMemo(
+    () =>
+      getIdFromShopifyEntityId(
+        SHOPIFY_ENTITY_TYPES.PRODUCT_VARIANT,
+        productVariant.id
+      ),
+    [productVariant]
+  )
+
   return (
-    <div
-      className={newReleasesItemStyle}
-      style={{
-        backgroundImage: `url(${productVariant.image?.url})`
-      }}
+    <a
+      href={`/products/${productVariant.product.handle}?variant=${productVariantId}`}
+      onClick={() =>
+        productVariantId && recordItemClickInteraction(productVariantId)
+      }
     >
-      <p className={newReleasesItemTitleStyle}>
-        {productVariant.product.title}
-      </p>
-    </div>
+      <div
+        className={newReleasesItemStyle}
+        style={{
+          backgroundImage: `url(${productVariant.image?.url})`
+        }}
+      >
+        <p className={newReleasesItemTitleStyle}>
+          {productVariant.product.title}
+        </p>
+      </div>
+    </a>
   )
 }
